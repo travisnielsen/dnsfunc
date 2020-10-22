5d092706f7de885e81ff21f524d3ca25f7b1ac5b
Travis Nielsen <trniel@microsoft.com>
2020-10-21 20:27:48 -0500

    error handling and minor updates

--- README.md
+++ README.md
@@ -8,21 +8,20 @@ This is a sample Azure Function that reports query time in milliseconds for one 
 
 Create Node JS Azure Function App. Ensure Application Insights is enabled as this will be used to capture DNS performance telelmetry. In the Function App, configure the following Application Settings:
 
-* `DNS_ZONES`: Comma-separated list of DNS A records
-* `CLOUDROLE_NAME`: Name of the Function App to be displayed in Application Insights
+`DNS_ZONES`: Comma-separated list of DNS A records
 
 By default, the timer trigger runs every 5 minutes.
 
 ## Deployment
 
-From the `src` directory, run `func azure functionapp publish [your_functionapp_name]` to deploy the Function code to an exisitng Function App.
+From the `src` directory, run `func azure functionapp publish [your_functionapp_name] --nozip` to deploy the Function code to an exisitng Function App.
 
 ## Reporting
 
-The Function App will submit custom telelmetry to Application Insights. From within the Logs section of Application Insights, you can create a chart to show DNS resolution performance with the following query:
+The Function App will submit custom telelmetry to the assigned Application Insights workspace. From within the Logs section of Application Insights, you can create a chart to show DNS resolution performance with the following query:
 
 ```bash
-customEvents 
+customEvents
 | extend zoneName = tostring(customDimensions["zoneName"]), queryTime = toint(customMeasurements["queryTime"])
 | summarize avg(queryTime) by zoneName, bin(timestamp, 5m)
 ```
--- src/checkZones/index.ts
+++ src/checkZones/index.ts
@@ -2,23 +2,25 @@ import { AzureFunction, Context } from "@azure/functions"
 import * as appInsights from "applicationinsights"
 import * as dns from "dns"
 
-const appInsightsKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY;
-const cloudRoleName = process.env.CLOUDROLE_NAME;
+const appInsightsKey = process.env["APPINSIGHTS_INSTRUMENTATIONKEY"];
 
 if (appInsightsKey) {
     console.log("App Insights key found. Initializing.")
     appInsights.setup(appInsightsKey).start();
-    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = cloudRoleName;
-    appInsights.defaultClient.context.tags["ai.cloud.role"] = cloudRoleName;
 }
 
 var telemetry = appInsights.defaultClient;
 
-
 const checkZones: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     
-    const domains = process.env.DNS_ZONES;
-    const zones = domains.split(",");
+    var domains = process.env["DNS_ZONES"];
+
+    try {
+        var zones = domains.split(",");
+    } catch (e) {
+        console.error(`No DNS records found. Please create an entry for DNS_ZONES in Application Settings. Separate each DNS zone with a comma.`);(1);
+        return;
+    }
 
     zones.forEach(function (zone) {
 
@@ -26,23 +28,25 @@ const checkZones: AzureFunction = async function (context: Context, myTimer: any
         
         dns.lookup(zone, (err, address, family) => {
 
-            var elapsed = new Date().getTime() - startTime;
-            console.log(zone, elapsed);
-            console.log('address: %j family: IPv%s', address, family);
-
-            let metrics = {};
-            metrics['queryTime'] = elapsed;
-
-            let props = {}
-            props['zoneName'] = zone;
-
-            telemetry.trackEvent({name: "dnsZoneQuery", measurements: metrics, properties: props })
+            if (err) {
+                console.error(`Error for query: ${zone} | ${err.message}`);
+            } else {
+                var elapsed = new Date().getTime() - startTime;
+                console.log(`Query: ${zone} | Time (ms): ${elapsed} | Address: ${address}`);
+    
+                let metrics = {};
+                metrics['queryTime'] = elapsed;
+    
+                let props = {}
+                props['zoneName'] = zone;
+    
+                telemetry.trackEvent({name: "dnsZoneQuery", measurements: metrics, properties: props })
+            }
 
           });
 
     });
 
-    // context.log('Timer trigger function ran!', timeStamp);   
 };
 
 export default checkZones;
